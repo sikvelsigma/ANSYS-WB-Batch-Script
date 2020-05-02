86e098c8c4569c75241db38f62abbd4ef079a3ed
sikvelsigma <63592183+sikvelsigma@users.noreply.github.com>
2020-05-02 04:30:45 +0300

    Update to main

--- run_script.py
+++ run_script.py
@@ -1,69 +1,96 @@
 # -*- coding: utf-8 -*-
 """ Script by Toybich Egor
 """
-import sys
-import os
+#__________________________________________________________
+#__________________________________________________________
 from glob import glob 
+import sys
+import platform
+import os
 
-# Dynamically import all the modules from working directory
 def find_module(st_in):
-	res = []
-	stlist = st_in if isinstance(st_in, list) else [st_in]
-	
-	for st in stlist:
-		try:
-			srch = [f for f in glob('{}*.py'.format(st))]
-			print('Found: {}'.format(srch))
-			srch = srch[0] if srch[0] == '{}.py'.format(st) else srch[-1]
-			srch = srch.replace('.py','')
-		except:
-			res.append(None)
-		else:
-			res.append(srch)
-	return tuple(res) if len(stlist) > 1 else res[0]
-	
+    res = []
+    stlist = st_in if isinstance(st_in, list) else [st_in]
+    
+    for st in stlist:
+        try:
+            srch = [f for f in glob('{}*.py'.format(st))]
+            print('Found: {}'.format(srch))
+            srch = srch[0] if srch[0] == '{}.py'.format(st) else srch[-1]
+            srch = srch.replace('.py','')
+        except:
+            res.append(None)
+        else:
+            res.append(srch)
+    return tuple(res) if len(stlist) > 1 else res[0]
+    
 
 modules = ['WBInterface', 'ExcelFileReader', 'Logger', 'CSVTable']
 modules_files = find_module(modules)
 
-print('Using: {}, {}, {}, {}'.format(*modules_files))
+print('Using: {}, {}, {}, {}'.format(*modules))
 
 if modules_files[0]: exec('from {} import WBInterface'.format(modules_files[0]))
 if modules_files[1]: exec('from {} import ExcelFileReader'.format(modules_files[1]))
 if modules_files[2]: exec('from {} import Logger'.format(modules_files[2]))
 if modules_files[3]: exec('import {} as CSVTable'.format(modules_files[3]))
 
-#__________________________________________________________
+#===========================================================================
 
 if __name__ == '__main__':
+    filepath = os.path.abspath(__file__)
+    filedir = os.path.dirname(filepath)
+    os.chdir(filedir)
+    print('CWD: ' + os.getcwd())
+    print('File: ' + filepath)
+    #__________________________________________________________
+    
+    wb = WBInterface()
 
-	# Main class to interact with ANSYS
-	wb = WBInterface()
+    try:
+        wb.open_archive()
+    except:
+        try:
+            wb.open_project()
+        except:
+            wb.log('Nothing to open!')
+            raise
 
-	# Firstly, try to open archive, then project file in cd
-	# method wb.log() is used to write to a log file
-	try:
-		wb.open_archive()
-	except:
-		try:
-			wb.open_project()
-		except:
-			wb.log('Nothing to open!')
-			raise
-
-	try:
-		wb.read_control()			# read .control file if exists
-		wb.read_input()				# read .input file if exists
-		wb.set_parameters()			# set parameters in Workbench project
-		wb.update_project()			# update Workbench project (auto-saved after)
-		wb.runtime()
-		wb.output_parameters()		# output parameters (auto-saved after)
-	except Exception as err_msg:
-		wb.log('CRITICAL ERROR')
-		wb.log(err_msg)
-	else:
-		wb.log('RUN SUCCESSFUL')
-	
-	wb.log('END SCRIPT')
-	wb.runtime()
+    try:
+        wb.read_control()
+        wb.read_input()
+        wb.set_parameters()
+        
+        """ Set number of cores to 6 or max available using system SYS
+        Replace 'SYS' with any other mechanical system.
+        This works only with new ribbon interface, which means ANSYS 2019R2 or higher!
+        """
+        # wb.set_cores_number('SYS', 6)   
+        
+        
+        """Turn on DMP solver using system SYS
+        Replace 'SYS' with any other mechanical system
+        This works only with new ribbon interface, which means ANSYS 2019R2 or higher!
+        """
+        # wb.set_distributed('SYS', True) 
+        
+        wb.update_project()
+        wb.runtime()
+        
+        """ Try to open SYS and save all Figures (NOT PLOTS!) from a project tree.
+        This includes all connected projects which share 'Model' since they are displayed 
+        in the same tree.
+        This was tested only on ANSYS with new ribbon interface!
+        """
+        # wb.save_figures('SYS', os.path.join(filedir, 'pictures'), width=1920, height=1080, fontfact=1.35)
+        
+        wb.output_parameters()
+    except Exception as err_msg:
+        wb.log('CRITICAL ERROR')
+        wb.log(err_msg)
+    else:
+        wb.log('RUN SUCCESSFUL')
+    
+    wb.log('END SCRIPT')
+    wb.runtime()
 
